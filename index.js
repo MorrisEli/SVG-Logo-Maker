01a03cb68afd65db4f860b34771a6f3a0b1ca99c
Eli Morris <elimorris@Elis-MBP.lan>
2023-04-01 13:53:57 -0700

    adding jest into package.json, laying out questions, and starting shapes.js

--- index.js
+++ index.js
@@ -3,11 +3,33 @@ const fs = require('fs')
 
 //shapes variable
 const { Triangle, Circle, Square} = require('./libs/shapes.js')
+const { type } = require('os')
 
 //questions
 function svgPrompt() {
     return inquirer
-    .prompt( {
+    .prompt([
+        {       //adding in choice for required shapes
+        type: 'list',
+        name: 'shape',
+        message: 'What shape do you want your svg logo to be?',
+        choices: ['Circle', 'Square', 'Triangle'],
+        },
+//background color
+        {
+            type: 'input',
+            name: 'backgroundColor',
+            message: 'What color would you like your shapes background to be?',
+            validate: function(input) {
+                if (input === ''){
+                    return
+                }
+            }
+        },
+//Text, three character length
 
-    })
+//Text color
+
+//
+    ])
 }
