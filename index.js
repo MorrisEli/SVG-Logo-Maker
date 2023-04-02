8e85e74993cef3615f0975a32c25c92b39ee41ee
Eli Morris <elimorris@Elis-MBP.lan>
2023-04-02 12:25:10 -0700

    index.js, shapes.test

--- index.js
+++ index.js
@@ -22,10 +22,37 @@ function svgPrompt() {
             message: 'What color would you like your shapes background to be?',
             validate: function(input) {
                 if (input === ''){
-                    return
+                    return `\x1b[31Enter a color\x1b[0m`
+                    }
+                    return true
+                }
+            
+            },
+            //Text, three character length
+            {
+                type: 'input',
+                name: 'svgText',
+                message: 'Enter your text. (up to 3 characters): ',
+                validate: function(input) {
+                    if (input.lenght > 3) { //make sure character length is < 3
+                        return
+                    }
+                }
+            },
+            //Text color
+            {
+                type: 'input',
+                name: 'textColor',
+                message: 'What would you like your text color to be?',
+                valide: function(input) {
+                    if (input === '') {
+                        return `\x1b[31mInput must be 3 characters or less\x1b[0m`
+                    }
+                    return true
                 }
             }
-        },
+
+        }
 //Text, three character length
 
 //Text color
---
+++ libs/shapes.js
@@ -0,0 +1,7 @@
+//shape class
+class shape {
+
+
+
+
+}
